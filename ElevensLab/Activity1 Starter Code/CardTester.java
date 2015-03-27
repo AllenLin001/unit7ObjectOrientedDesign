d30e84c149d90c28447f7ffe574e05b892fb7cdd
AllenLin001 <allenlin41@yahoo.com>
2015-03-26 21:15:22 -0500

    Done

--- ElevensLab/Activity1 Starter Code/CardTester.java
+++ ElevensLab/Activity1 Starter Code/CardTester.java
@@ -3,11 +3,19 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) 
+    {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        Card c1 = new Card("Jack","Spade",11);
+        Card c2 = new Card("King","Clubs",13);
+        Card c3 = new Card("Queen","Heart",12);
+        System.out.println("It should be Spade");
+        c3.suit();
+
+
+    }
 }
