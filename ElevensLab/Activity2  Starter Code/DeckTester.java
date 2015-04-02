fdf5ccd6f04deb6564d10d8afe3f6b9de08b7a76
AllenLin001 <allenlin41@yahoo.com>
2015-04-02 08:07:24 -0500

    Done

--- ElevensLab/Activity2  Starter Code/DeckTester.java
+++ ElevensLab/Activity2  Starter Code/DeckTester.java
@@ -9,5 +9,13 @@ public class DeckTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks = {"3", "4", "5"};
+        String[] suits = {"blue", "red"};
+        int[] pointValues = {11, 12, 13};
+        Deck deck1 = new Deck(ranks, suits, pointValues);
+        
+        System.out.println("  toString:\n" + deck1.size());
+        System.out.println("  isEmpty: " + deck1.isEmpty());
+		System.out.println("  size: " + deck1.toString());
 	}
 }
